3aaac96c33ae51b204a04926e41f01fa5d00e97b
Shrutika <shrutika.rkt@gmail.com>
2020-07-13 22:55:17 +0530

    worked on links
    some links left

--- HackOwasp/CodeDigger/settings.py
+++ HackOwasp/CodeDigger/settings.py
@@ -123,5 +123,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+ 
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR, 'static')
+ ]
 
+STATIC_ROOT= os.path.join(BASE_DIR, 'assets')
 
